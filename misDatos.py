e1aaa9ce0948927a8a358d816cddc798cb55d054
A1376508 <61155120+A1376508@users.noreply.github.com>
2020-02-29 03:08:07 -0600

    Misión 2, Fernando Pérez, A01376508
    No se si le esta llegando mi request, tengo miedo así que lo volveré a mandar :) 
    También tuve que hacer de nuevo los archivos de Thonny porque no me dejaba usarlos

--- misDatos.py
+++ misDatos.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Fernando Pérez González, A01376508
+# Descripcion: Elaborar un algoritmo que muestre mis datos
 
 # Escribe tu programa después de esta línea.
+
+print ("Mi nombre es: ")
+print ("Fernando Pérez González")
+print ("Tenga la matrícula: ")
+print ("A01376508")
+print ("Mi carrera es: ")
+print ("LCMD")
+print ("Mi escuela de procedencia es: ")
+print ("Prepa Tec, programa Multicultural")
+print ("Descripción: ")
+print ("Me gusta el cine, más que nada la dirección, arte y fotografía.")
+print ("Practico natación desde que tenìa un año de edad y amo ir a conciertos.")
