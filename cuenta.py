9c23f6d15f9f5ad6993fbfdd24f854c93d22900b
A1376508 <61155120+A1376508@users.noreply.github.com>
2020-03-05 09:57:34 -0600

    Fernando Pérez, A01376508
    Subiendo tarde porque los archivos que mande estaban vacíos

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Fernando Pérez González, A01376508
+# Descripcion: Elaborar un algoritmo que muestre el subtotal y el costo total de una comida
 
-# Escribe tu programa después de esta línea.
+# Escribe tu programa después de esta línea
+
+costo = float(input("Teclea el costo de tu comida: "))
+
+propina= (costo*13)/100
+IVA = (costo*16)/100
+total = costo + propina + IVA
+
+print("El subtotal de la comida es de: $%.2f  " % costo)
+print("La propina de la comida es de: $%.2f " % propina)
+print("El IVA de la comida es de: $%.2f  " % IVA)
+print("El total de la comida es de $%.2f " % total)
